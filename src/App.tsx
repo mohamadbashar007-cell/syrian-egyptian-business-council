bda917741aeaa82ca8c7f5831a1c7378ee034f00
Mohamad Bashar <mohamad.bashar007@gmail.com>
2026-07-16 22:56:27 +0300

    Fix GitHub Pages router basename

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import Contact from './pages/Contact';
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
